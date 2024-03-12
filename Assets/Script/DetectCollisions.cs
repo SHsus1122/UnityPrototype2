995cb77a9272e219ece0f44ef1805c9876783f32
SHsus1122 <tjgurtn1122@naver.com>
2024-03-12 16:20:38 +0900

    물리 충돌시 삭제 구현

---
+++ Assets/Script/DetectCollisions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 유니티 기본 제공 함수를 사용합니다.
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);        // 자기 자신을 삭제
+        Destroy(other.gameObject);  // 충돌한 객체를 삭제
+    }
+}
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -10,23 +10,29 @@ public class SpawnManager : MonoBehaviour
     //public int animalIndex;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay= 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 호출할 메서드를 사용해 특정 시점에 해당 메서드의 호출을 시작하고 시간 경과에 따라 원하는 속도로 호출을 반복합니다.
+        // 인자 설명 : 호출할 함수명, 시작 시간, 반복 주기
+        // 주의할 점으로 함수명은 대소문자까지 정확히 일치해야 합니다.
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // S 키를 눌러서 배열안의 녀석들 중 에디터에서 고른 녀석을 출력
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);        // 이 프로젝트 상에서는 0 ~ 2 범위의 랜덤 숫자
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);   // 생성 동물, 위치 랜덤으로 변경
+        
+    }
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);        // 이 프로젝트 상에서는 0 ~ 2 범위의 랜덤 숫자
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);   // 생성 동물, 위치 랜덤으로 변경
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
